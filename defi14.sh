1acdaaa1a4afd7d4439e86cea872437330d72f87
EnzDev <enzo@mallard.ml>
2016-01-06 16:09:39 +0100

    defi 14

--- defi14.sh
+++ defi14.sh
@@ -4,16 +4,18 @@ cd $1
 fold=`ls -1AF|grep "/$"|tr -d "/$" `
 files=`find -maxdepth 1 -type f|sed "s+^./++g"`
 
+out="\033[92m\tDossiers\n"
 
 for item in $fold; do
-out=$out"$item \\ \033[32m `ls $item |grep "" -c` \033[0m"
+out=$out"\033[32m$item \\ `ls $item |grep "" -c`\033[0m"
 out=$out$"\n"
 done
-out=$out$"\n"
+out=$out$"\n\033[94m\tFichiers\n"
 
 for item in $files; do
-out=$out"$item \\ \033[33m `du -h $item|cut -f1` \033[0m"
+out=$out"\033[34m$item \\ `du -h $item|cut -f1`\033[0m"
 out=$out$"\n"
 done
+out=$out"\033[0m"
 echo -e $out|column -s "\\" -t
 
