df1d904a9c3e1926979a942d59d419be25afc2b2
EnzDev <enzo@mallard.ml>
2016-01-06 15:40:34 +0100

    defi 14

---
+++ defi14.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+IFS=$'\n'
+cd $1
+fold=`ls -1AF|grep "/$"|tr -d "/$" `
+files=`find -maxdepth 1 -type f|sed "s+^./++g"`
+#echo -e "\033[31m"
+#echo $fold
+#echo -e "\033[32m"
+#echo $files
+#echo -e "\033[0m"
+
+for item in $fold; do
+out=$out"$item \\ \033[32m `ls $item |grep "" -c` \033[0m"
+out=$out$"\n"
+done
+out=$out$"\n"
+
+for item in $files; do
+out=$out"$item \\ \033[33m `du -h $item|cut -f1` \033[0m"
+out=$out$"\n"
+done
+echo -e $out|column -s "\\" -t
+
